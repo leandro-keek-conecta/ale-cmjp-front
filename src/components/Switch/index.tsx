b385c38a132da67d580f39aa8b2fb3cc70d98fed
leandro-keek-conecta <leandro@keekconecta.com.br>
2026-02-11 12:40:57 -0300

    Swith atualizado para garantir que o componente se adeque melhor aos formulários

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -16,17 +16,41 @@ export default function SwitchField({
   helperText,
 }: SwitchFieldProps) {
   return (
-    <Box>
-      <FormControlLabel
-        control={
-          <Switch
-            checked={checked}
-            onChange={(_, value) => onChange(value)}
-            color="primary"
-          />
-        }
-        label={label}
-      />
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <Box
+        sx={{
+          width: "100%",
+          minHeight: "32px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <FormControlLabel
+          sx={{
+            width: "100%",
+            margin: 0,
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+          labelPlacement="start"
+          control={
+            <Switch
+              checked={checked}
+              onChange={(_, value) => onChange(value)}
+              color="primary"
+            />
+          }
+          label={label}
+        />
+      </Box>
       {helperText ? (
         <FormHelperText error={error}>{helperText}</FormHelperText>
       ) : null}
